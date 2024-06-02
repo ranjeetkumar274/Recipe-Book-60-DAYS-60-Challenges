0843a84bcbbac7f7681f286f2effb78b038be698
RANJEET KUMAR <147934359+Ashuuydv@users.noreply.github.com>
2024-06-02 19:35:55 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
 const API_KEY = "4aa61360a5d34607907c785fdbab67c8";
 const recipelistel = document.getElementById("recipe-list");
 
-async function getRecipes() {
-        const response = await fetch(`https://api.spoonacular.com/recipes/random?number=10&apiKey=${API_KEY}`);
+function displayRecipes(recipes){
+    recipelistel.innerHTML = "";
+    recipes.forEach((recipe) => {
+        const recipeItemEl = document.createElement("li");
+        recipeItemEl.classList.add("recipe-item");
         
+        const recipeImageEl = document.createElement("img");
+        recipeImageEl.src = recipe.image;
+        recipeImageEl.alt = "recipe image";
+
+        recipeTitleEl = document.createElement("h2");
+        recipeTitleEl.innerText =recipe.title;
         
-        const data = await response.json();
-        return data.recipes;
+        recipeItemEl.appendChild(recipeImageEl);
+        recipeItemEl.appendChild(recipeTitleEl);
+        recipelistel.appendChild(recipeItemEl);
+    });
 }
 
-function displayRecipes(recipes){
-    recipelistel.innerHTML = ""
-    recipes.forEach()
+async function getRecipes() {
+    const response = await fetch(`https://api.spoonacular.com/recipes/random?number=10&apiKey=${API_KEY}`);
+    const data = await response.json();
+    return data.recipes;
 }
 
 async function init() {
-    const recipes = await getRecipes();
-    if (recipes) {
-        displayRecipes();
+    try {
+        const recipes = await getRecipes();
+        if (recipes) {
+            displayRecipes(recipes); // Pass the recipes array to the displayRecipes function
+        }
+    } catch (error) {
+        console.error("Error fetching recipes:", error);
     }
 }
 
